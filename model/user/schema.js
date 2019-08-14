164efcaff4f2ae42556f83918602da6909b46ce2
Ayako Sayama <ayakosayama1987@gmail.com>
2019-08-14 00:12:48 -0700

    remove the uri

---
+++ model/user/schema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true },
+  gender: { type: String, required: true },
+  ip_address: { type: String, required: true },
+  last_name: { type: String }
+})
+
+module.exports = userSchema;
